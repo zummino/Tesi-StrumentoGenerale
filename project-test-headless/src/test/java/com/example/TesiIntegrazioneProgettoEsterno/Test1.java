c8c5bf18c044827e63bf2fb2bd103d061e859689
gianlucat97 <84741536+gianlucat97@users.noreply.github.com>
2022-02-03 01:00:02 +0100

    Adding project-test-headless idealmente funzionante

---
+++ project-test-headless/src/test/java/com/example/TesiIntegrazioneProgettoEsterno/Test1.java
@@ -0,0 +1,78 @@
+package com.example.TesiIntegrazioneProgettoEsterno;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class Test1 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\Downloads\\chromedriver_win32_ChromeV97\\chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void test1() throws Exception {
+    driver.get("http://www.uni2na.it/chi-siamo/cenni-storici");
+    driver.findElement(By.id("ateneo")).click();
+    driver.findElement(By.xpath("//nav[@id='sidebar-navigation']/ul/li[5]/span")).click();
+    driver.findElement(By.xpath("//nav[@id='sidebar-navigation']/ul/li[3]/span")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
