327e7120b2e068edf31b5fdccc5f0aef518542c7
gianlucat97 <84741536+gianlucat97@users.noreply.github.com>
2022-02-06 17:23:08 +0100

    Settaggio Ambiente generico

--- startBackEnd.sh
+++ startBackEnd.sh
@@ -1,7 +1,1 @@
-cd /home/runner/work/Tesi-integrazioneProgettoEsterno/Tesi-integrazioneProgettoEsterno/angular-java-example-master
-mvn clean install
-cd /home/runner/work/Tesi-integrazioneProgettoEsterno/Tesi-integrazioneProgettoEsterno/angular-java-example-master/target
-echo "Vediamo quali file jar si trovano in cartella target"
-ls -a
-echo "Proviamo ad eseguire ${{ secrets.NOME_JAR_WEBAPP }}.jar"
-java -jar users-0.0.1-SNAPSHOT.jar
+echo "inserisci qui i comandi per lo starting del Back-End della tua applciazione web"
--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -1,5 +1,1 @@
-cd /home/runner/work/Tesi-integrazioneProgettoEsterno/Tesi-integrazioneProgettoEsterno/angular-java-example-master/src/main/ui
-echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
-npm install
-echo "Installazione npm effettuata, prossimo comando: npm start"
-npm start
+echo "inserisci qui i comandi per lo starting del Front-End della tua applciazione web"
