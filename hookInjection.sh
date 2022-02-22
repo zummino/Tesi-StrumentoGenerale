e5c52422d7e90f4bfd82d5112cbad0de8ca6df6f
gianlucat97 <84741536+gianlucat97@users.noreply.github.com>
2022-02-22 23:50:17 +0100

    Delete hookInjection.sh

--- hookInjection.sh
+++
@@ -1,6 +0,0 @@
-echo "Compilare il seguente script con i comandi per effettaure l'injection per ognuno dei file di FE che si vuole instrumentare. Di sotto degli esempi stampati a video tramite il comando echo:"
-
-echo "node main.js inject-hooks /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/rootWebApp/file1.html --grammar angularjs"
-echo "node main.js inject-hooks /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/rootWebApp/file2.html --grammar angularjs"
-echo "node main.js inject-hooks /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/rootWebApp/innerDirectory/file3.html --grammar angularjs"
-
