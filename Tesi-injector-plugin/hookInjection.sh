3a604dde8e590d66db303db762611b15f08a7f26
gianlucat97 <84741536+gianlucat97@users.noreply.github.com>
2022-02-23 03:25:07 +0100

    Delete hookInjection.sh

--- Tesi-injector-plugin/hookInjection.sh
+++
@@ -1,7 +0,0 @@
-node main.js inject-hooks C:\Users\talit\Desktop\test-hooks-master\test-hooks-master\frontend\index.html --grammar angularjs
-node main.js inject-hooks C:\Users\talit\Desktop\test-hooks-master\test-hooks-master\frontend\templates\calendarDirective.html --grammar angularjs
-node main.js inject-hooks C:\Users\talit\Desktop\test-hooks-master\test-hooks-master\frontend\templates\embeddedTemplate.html --grammar angularjs
-node main.js inject-hooks C:\Users\talit\Desktop\test-hooks-master\test-hooks-master\frontend\templates\mainState.html --grammar angularjs
-node main.js inject-hooks C:\Users\talit\Desktop\test-hooks-master\test-hooks-master\frontend\templates\taskEditState.html --grammar angularjs
-node main.js inject-hooks C:\Users\talit\Desktop\test-hooks-master\test-hooks-master\frontend\templates\tasksState.html --grammar angularjs
-node main.js inject-hooks C:\Users\talit\Desktop\test-hooks-master\test-hooks-master\frontend\templates\usersState.html --grammar angularjs
