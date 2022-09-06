cfe94b0cd0923cd2e8d7d9454e9d0a3e43a847b6
zummino <marcodeluca94@gmail.com>
2022-09-06 07:56:04 +0000

    push automatico per la correzione del formato dei test

--- project-test-headless/src/test/java/com/example/TesiIntegrazioneProgettoEsterno/Test4Tradizionale.java
+++ project-test-headless/src/test/java/com/example/TesiIntegrazioneProgettoEsterno/Test4Tradizionale.java
@@ -1,31 +1,37 @@
-package com.example.TestSuiteProvaSpost2;
 
-import java.util.regex.Pattern;
+//File risulta attualmente aggiornato per webdriver chrome headless!
+package com.example.TesiIntegrazioneProgettoEsterno;
+
 import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
+
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.apache.commons.io.FileUtils;
-import java.io.File;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Test4Tradizionale {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
-  JavascriptExecutor js;
-  @Before
-  public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver", "");
-    driver = new ChromeDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-    js = (JavascriptExecutor) driver;
-  }
+private static WebDriver driver;
+private boolean acceptNextAlert = true;
+private static StringBuffer verificationErrors = new StringBuffer();
 
+	  @Before
+	  public void setUp() throws Exception {
+		
+		  // Init chromedriver
+		  String chromeDriverPath = "/home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/chromedriver_v94_linux64/chromedriver";
+		  System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		  System.setProperty("webdriver.chrome.whitelistedIps", "");
+		  ChromeOptions options = new ChromeOptions();
+		  options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--no-sandbox","--ignore-certificate-errors");
+		  driver = new ChromeDriver(options);  
+		  
+		  
+		  
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
   @Test
   public void testTradizionale_v_2_1() throws Exception {
     driver.get("http://localhost:4200/");
@@ -43,45 +49,47 @@ public class Test4Tradizionale {
     driver.findElement(By.xpath("//a[contains(text(),'Gemitaiz')]")).click();
   }
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+ @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
+	  }
+
 }
