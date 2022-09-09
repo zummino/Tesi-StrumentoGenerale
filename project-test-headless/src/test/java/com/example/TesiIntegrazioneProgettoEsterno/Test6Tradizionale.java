7f518d3af5972b6a770cdca7b83943a5a75399ba
zummino <marcodeluca94@gmail.com>
2022-09-09 10:11:19 +0200

    fixed test_case n 5/6 (v_3_0 e v_3_1)

--- project-test-headless/src/test/java/com/example/TesiIntegrazioneProgettoEsterno/Test6Tradizionale.java
+++ project-test-headless/src/test/java/com/example/TesiIntegrazioneProgettoEsterno/Test6Tradizionale.java
@@ -1,95 +1,83 @@
+package com.example.TestSuiteMoveTmplt2;
 
-//File risulta attualmente aggiornato per webdriver chrome headless!
-package com.example.TesiIntegrazioneProgettoEsterno;
-
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.*;
-
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
 
 public class Test6Tradizionale {
-private static WebDriver driver;
-private boolean acceptNextAlert = true;
-private static StringBuffer verificationErrors = new StringBuffer();
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    js = (JavascriptExecutor) driver;
+  }
 
-	  @Before
-	  public void setUp() throws Exception {
-		
-		  // Init chromedriver
-		  String chromeDriverPath = "/home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/chromedriver_v94_linux64/chromedriver";
-		  System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-		  System.setProperty("webdriver.chrome.whitelistedIps", "");
-		  ChromeOptions options = new ChromeOptions();
-		  options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--no-sandbox","--ignore-certificate-errors");
-		  driver = new ChromeDriver(options);  
-		  
-		  
-		  
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  }
   @Test
   public void testTradizionale_v_3_1() throws Exception {
     driver.get("http://localhost:4200/");
     driver.findElement(By.id("login-username")).click();
     driver.findElement(By.id("login-username")).clear();
     driver.findElement(By.id("login-username")).sendKeys("odoobnb@gmail.com");
-    driver.findElement(By.id("login-password")).click();
     driver.findElement(By.id("login-password")).clear();
     driver.findElement(By.id("login-password")).sendKeys("provaprova93");
-    driver.findElement(By.xpath("//button[@id='login-button']/div")).click();
-    driver.findElement(By.xpath("//as-nav-bar-playlist/ul/li/a")).click();
-    driver.findElement(By.xpath("//input")).click();
-    driver.findElement(By.xpath("//input")).clear();
-    driver.findElement(By.xpath("//input")).sendKeys("ciao");
-    driver.findElement(By.xpath("//a[contains(text(),'Gemitaiz')]")).click();
+    driver.findElement(By.xpath("//button[@id='login-button']/div/p")).click();
+    driver.findElement(By.xpath("//li[6]/a")).click();
+    driver.findElement(By.xpath("//*/text()[normalize-space(.)='Top list']/parent::*")).click();
   }
 
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
 
- @After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	    String verificationErrorString = verificationErrors.toString();
-	    if (!"".equals(verificationErrorString)) {
-	      fail(verificationErrorString);
-	    }
-	  }
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
 
-	  private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
 
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
-
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
-
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
 }
