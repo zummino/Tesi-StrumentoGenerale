2150cc6551e58546449bc84925e0f874ae85e604
zummino <71648648+zummino@users.noreply.github.com>
2023-02-21 12:28:25 +0100

    Update startFrontEnd.sh

--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -15,7 +15,8 @@ echo "Inizio comandi esecuzione Frontend"
 cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-spotify-main/angular-spotify-main
 echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
 #npm install
-npm list
+npm ci
+#npm list
 echo "Installazione npm effettuata, prossimo comando: npm start"
 echo "npm start"
 npm start > /dev/null 2>&1 &
